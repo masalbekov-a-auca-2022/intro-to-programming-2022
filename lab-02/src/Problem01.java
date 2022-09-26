aebe68a96606fcb4318669dd30faf633e6dca460
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-26 18:10:31 +0600

    Problem1

--- lab-02/src/Problem01.java
+++ lab-02/src/Problem01.java
@@ -4,14 +4,14 @@ public class Problem01 {
     public static void main(String[] args) {
         System.out.print("1st value? ");
         Scanner num = new Scanner(System.in);
-        int number = num.nextInt();
+        int a = num.nextInt();
         System.out.print("2nd value? ");
-        int number2 = num.nextInt();
-        System.out.println("Before swapping: a = " + number + "; b = " + number2 + ";");
-        int temp = number;
-        number = number2;
-        number2 = temp;
-        System.out.println("After swapping: a = " + number + "; b = " + number2 + ";");
+        int b = num.nextInt();
+        System.out.println("Before swapping: a = " + a + "; b = " + b + ";");
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("After swapping: a = " + a + "; b = " + b + ";");
 
 
     }
