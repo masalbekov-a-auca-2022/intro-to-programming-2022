1ab154c3582dbfeed333bf649825958c5be11744
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-26 17:37:10 +0600

    Problem07

--- lab-01/src/Problem07.java
+++ lab-01/src/Problem07.java
@@ -1,9 +1,14 @@
+
 public class Problem07 {
     public static void main(String[] args) {
-        System.out.println("a  a^2 a^3");
-        System.out.println("1  1   1");
-        System.out.println("2  4   8");
-        System.out.println("3  9   27");
-        System.out.println("4  16  64");
+        int number = 1;
+        int number2 = 2;
+        int number3 = 3;
+        int number4 = 4;
+        System.out.println("a    a^2   a^3");
+        System.out.println(number + "    "  + (number*number) + "     " + (number*number*number));
+        System.out.println(number2 + "    "  + (number2*number2) + "     " + (number2*number2*number2));
+        System.out.println(number3 + "    "  + (number3*number3) + "     " + (number3*number3*number3));
+        System.out.println(number4 + "    "  + (number4*number4) + "    " + (number4*number4*number4));
     }
 }
