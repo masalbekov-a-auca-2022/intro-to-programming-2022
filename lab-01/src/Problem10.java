ae42d5e5ad3e08c69bd0555d5fc48b946007120c
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-26 17:52:36 +0600

    Problem10

--- lab-01/src/Problem10.java
+++ lab-01/src/Problem10.java
@@ -1,2 +1,17 @@
 public class Problem10 {
+    public static void main(String[] args) {
+        double a = 31536000;
+        double b = 312032486;
+        b = b + (a/7.0) + (a/45.0) - (a/13.0);
+        System.out.println(Math.round(b));
+        b = b + (a/7.0) + (a/45.0) - (a/13.0);
+        System.out.println(Math.round(b));
+        b = b + (a/7.0) + (a/45.0) - (a/13.0);
+        System.out.println(Math.round(b));
+        b = b + (a/7.0) + (a/45.0) - (a/13.0);
+        System.out.println(Math.round(b));
+        b = b + (a/7.0) + (a/45.0) - (a/13.0);
+        System.out.println(Math.round(b));
+        
+    }
 }
