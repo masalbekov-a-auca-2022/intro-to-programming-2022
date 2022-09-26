5d6e8b413a18efd598db680f478fbc611264c0f7
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-26 17:23:53 +0600

    Problem05

--- lab-01/src/Problem05.java
+++ lab-01/src/Problem05.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Problem05 {
+    public static void main(String[] args) {
+        System.out.print("1st double? ");
+        Scanner intg = new Scanner(System.in);
+        double number = intg.nextDouble();
+        System.out.print("2nd double? ");
+        double number2 = intg.nextDouble();
+        System.out.println(number + " + " + number2 + " = " + (number + number2));
+        System.out.println(number + " - " + number2 + " = " + (number - number2));
+        System.out.println(number + " * " + number2 + " = " + (number * number2));
+        System.out.println(number + " / " + number2 + " = " + (number / number2));
+        System.out.println(number + " % " + number2 + " = " + (number % number2));
+
+    }
 }
