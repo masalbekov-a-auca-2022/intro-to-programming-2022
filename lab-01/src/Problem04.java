0f37921011d71cc4c8f673a8ff54ec79b6ce4058
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-26 17:20:37 +0600

    Problem04

--- lab-01/src/Problem04.java
+++ lab-01/src/Problem04.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Problem04 {
+    public static void main(String[] args) {
+        System.out.print("1st int? ");
+        Scanner intg = new Scanner(System.in);
+        int number = intg.nextInt();
+        System.out.print("2st int? ");
+        int number2 = intg.nextInt();
+        System.out.println(number + " + " + number2 + " = " + (number + number2));
+        System.out.println(number + " - " + number2 + " = " + (number - number2));
+        System.out.println(number + " * " + number2 + " = " + (number * number2));
+        System.out.println(number + " / " + number2 + " = " + (number / number2));
+        System.out.println(number + " % " + number2 + " = " + (number % number2));
+
+    }
 }
