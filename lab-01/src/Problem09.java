2dd69208e9139b9ba768416afe1926826ae1a961
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-26 17:43:54 +0600

    Problem09

--- lab-01/src/Problem09.java
+++ lab-01/src/Problem09.java
@@ -1,2 +1,7 @@
 public class Problem09 {
+    public static void main(String[] args) {
+        System.out.println(4*(1-1/3.0 + 1/5.0 - 1/7.0 + 1/9.0 - 1/11.0));
+        System.out.println(4*(1-1/3.0 + 1/5.0 - 1/7.0 + 1/9.0 - 1/11.0 + 1/13.0));
+
+    }
 }
