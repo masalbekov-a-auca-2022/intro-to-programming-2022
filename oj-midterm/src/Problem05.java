5c1a34ef174363eb4ccdeef8b4584d2a5f09eaab
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-30 15:53:55 +0600

    OJ 05 Accepted

---
+++ oj-midterm/src/Problem05.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println(num.nextInt() * 8 + num.nextInt() * 3 - 28);
+    }
+}
