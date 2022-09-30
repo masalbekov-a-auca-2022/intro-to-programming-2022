c05c4c50a81116a817a5d7638909973b1e1ab310
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-09-30 15:37:17 +0600

    OJ 02 Accepted

---
+++ oj-midterm/src/Problem02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+        int b = num.nextInt();
+        System.out.println(a + b);
+    }
+}
