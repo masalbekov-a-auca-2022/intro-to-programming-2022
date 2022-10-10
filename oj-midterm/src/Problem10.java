21cb8b2b876c6c248cd1b2fa252294cf7a8258ab
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-10-10 14:01:22 +0600

    OJ 10 Accepted

---
+++ oj-midterm/src/Problem10.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int v = num.nextInt();
+        System.out.println(v/4.9);
+
+    }
+}
