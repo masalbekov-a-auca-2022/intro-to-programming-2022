1bf64e3baab7328e95fa4000ae863f4ee9e0628f
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-10-10 13:30:54 +0600

    OJ 06 Accepted

--- oj-midterm/src/Problem06.java
+++ oj-midterm/src/Problem06.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class Problem06 {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        System.out.print("Type width and length:");
         int width = num.nextInt();
         int length = num.nextInt();
-        System.out.print("Type tile side: ");
         int tile_side = num.nextInt();
         int amount_x = width/tile_side;
         int amount_y = length/tile_side;
         int tile_amount = amount_x*amount_y;
         System.out.println(tile_amount);
-
     }
 }
