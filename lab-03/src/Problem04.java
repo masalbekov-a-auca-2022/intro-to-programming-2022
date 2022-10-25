9397c753b5c0a41fce9a5b42bfc290969635decc
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-10-25 13:15:16 +0600

    Problem04

--- lab-03/src/Problem04.java
+++ lab-03/src/Problem04.java
@@ -5,9 +5,7 @@ public class Problem04 {
         Scanner num = new Scanner(System.in);
         System.out.print("Year? ");
         int year = num.nextInt();
-        int length = (int) (Math.log10(year) + 1);
-        int y = (length / 100)*100;
-        if (year % 100 == 0 || (year - y) % 4 == 0 ) {
+        if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0) {
             System.out.println("This is a leap year.");
         } else {
             System.out.println("This is not a leap year.");
