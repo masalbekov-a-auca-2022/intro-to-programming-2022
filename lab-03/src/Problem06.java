1b19bca29db9a6d11645461531c5fefa5ea6820e
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-10-25 14:02:20 +0600

    Problem 06

--- lab-03/src/Problem06.java
+++ lab-03/src/Problem06.java
@@ -9,9 +9,9 @@ public class Problem06 {
         double c = num.nextDouble();
         double dis = Math.sqrt(b * b - 4 * a * c);
         if (dis > 0) {
-            double first_root = (-b + dis) / 2 * a;
-            double second_root = (-b - dis) / 2 * a;
-            System.out.printf("The equation has two roots %.4f and %.4f\n", first_root, second_root);
+            double root1 = (-b + dis) / 2 * a;
+            double root2 = (-b - dis) / 2 * a;
+            System.out.printf("The equation has two roots %.4f and %.4f\n", root1, root2);
         } else if (dis == 0) {
             double root = -b / 2 * a;
             System.out.printf("The equation has one root %.4f\n", root);
