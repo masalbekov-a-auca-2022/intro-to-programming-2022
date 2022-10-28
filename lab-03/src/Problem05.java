261662a937d202d1f3d24779886211e49c6155cf
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-10-28 12:17:19 +0600

    Problem05

--- lab-03/src/Problem05.java
+++ lab-03/src/Problem05.java
@@ -4,21 +4,23 @@ public class Problem05 {
     public static void main(String[] args) {
         System.out.print("Number of points? ");
         Scanner scanner = new Scanner(System.in);
-        String g = "";
+        String g;
         int grade = scanner.nextInt();
-        if (grade > 100 || grade < 0){
-            System.out.println(grade + " is not in the permitted range.");}
-        else if (grade >= 90){
+        if (grade >= 90) {
             g = "A";
-        } else if (grade >= 80){
+        } else if (grade >= 80) {
             g = "B";
-        } else if (grade >= 70){
+        } else if (grade >= 70) {
             g = "C";
-        } else if (grade >= 60){
+        } else if (grade >= 60) {
             g = "D";
-        } else{
+        } else {
             g = "F";
         }
-        System.out.println("Grade: " + g);
+        if (grade < 100) {
+            System.out.println("Grade: " + g);
+        } else {
+            System.out.println(grade + " is not in the permitted range.");
+        }
     }
 }
