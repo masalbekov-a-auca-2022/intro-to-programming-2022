60b39129e1c63bc0a305d83666a60219bf5883da
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-10-31 17:00:32 +0600

    Problem09

---
+++ lab-03/src/Problem09.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Problem09 {
+    public static void main(String[] args) {
+        System.out.print("scissor (0), rock (1), paper (2): ");
+        Scanner scanner = new Scanner(System.in);
+        int player = scanner.nextInt();
+        Random rand = new Random();
+        int computer = rand.nextInt(3);
+        System.out.printf("The computer is %s. You are %s. ", nameofShape(computer), nameofShape(player));
+        if (player == 0 && computer == 2) {
+            System.out.println("You won");
+        } else if (computer == 0 && player == 2) {
+            System.out.println("Computer won");
+        } else if (player < computer) {
+            System.out.println("Computer won");
+        } else if (computer < player) {
+            System.out.println("You won");
+        } else {
+            System.out.println("It is a draw");
+        }
+    }
+
+    public static String nameofShape(int n) {
+        return switch (n) {
+            case 0 -> "scissor";
+            case 1 -> "rock";
+            case 2 -> "paper";
+            default -> null;
+        };
+    }
+}
+
+
