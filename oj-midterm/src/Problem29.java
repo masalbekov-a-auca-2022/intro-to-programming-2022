2d5ad264d43bde1ccb9cbaa51f25d2f56e35fbed
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-01 20:57:33 +0600

    OJ 29 Accepted

---
+++ oj-midterm/src/Problem29.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
+        int mid = a + b + c - max - min;
+        System.out.println(mid);
+    }
+}
