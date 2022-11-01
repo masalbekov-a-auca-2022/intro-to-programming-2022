89f615e369ee4492ebc542d22890401a8a620a32
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-01 20:13:41 +0600

    OJ 15 Accepted

---
+++ oj-midterm/src/Problem15.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int e = scanner.nextInt();
+        System.out.println(5*a + 10*b + 25*c +100*d +200*e);
+    }
+}
