8bc45339aaf7749125dc195263f5c78638374466
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-21 20:01:33 +0600

    OJ FINAL 04 ACCEPTED

---
+++ oj-final/src/Problem04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+        for (int i = 0; i < days; i++){
+            int c = scanner.nextInt();
+            int time = scanner.nextInt();
+            int answer = ((time*(time+1)/2)+time);
+            System.out.println(c +" " + answer);
+        }
+    }
+}
