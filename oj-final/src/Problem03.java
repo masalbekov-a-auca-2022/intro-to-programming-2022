840092fc4c3ce37cca7dbe247b38ce177d652be5
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-21 18:51:52 +0600

    OJ FINAL 03 ACCEPTED

---
+++ oj-final/src/Problem03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int bats = 0;
+        double score = 0;
+        int amount = scanner.nextInt();
+        for (int i =0; i < amount; i++){
+            int score1 = scanner.nextInt();
+            if(score1<0) continue;
+            bats++;
+            score +=score1;
+        }
+        System.out.println(score/bats);
+    }
+}
