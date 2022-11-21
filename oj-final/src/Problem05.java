25eb70413c68d625f736ae82387154e5ed9e2c4b
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-21 19:37:42 +0600

    OJ FINAL 05 ACCEPTED

--- oj-final/src/Problem05.java
+++ oj-final/src/Problem05.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Problem05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int total = 0;
         int g = scanner.nextInt();
         int t = scanner.nextInt();
+        int total = 0;
         int amount = scanner.nextInt();
-        for(int i = 0; i < amount; i++){
-
-
+        for (int i = 0; i < amount; i++) {
+            int item = scanner.nextInt();
+            total += item;
         }
+        System.out.println((int) ((g - t) * 0.9 - total));
     }
 }
