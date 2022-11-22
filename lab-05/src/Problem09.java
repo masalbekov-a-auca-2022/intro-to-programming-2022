8c7e1c947375618140c2ed83a3088fa2b04f5b1b
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-22 23:47:00 +0600

    Problem09

--- lab-05/src/Problem09.java
+++ lab-05/src/Problem09.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Problem09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string ");
+        String word = sc.nextLine();
+        int first = 0;
+        int last = word.length() - 1;
+        boolean isPalindrome = true;
+        while (first < last) {
+            if (word.charAt(first) != word.charAt(last)) {
+                isPalindrome = false;
+                break;
+            }
+            first++;
+            last--;
+        }
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
 }
