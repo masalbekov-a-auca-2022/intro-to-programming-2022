aff3d411f1d24dcc39e223dd664c66566a7392f6
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-22 23:47:09 +0600

    Problem10

--- lab-05/src/Problem10.java
+++ lab-05/src/Problem10.java
@@ -1,2 +1,26 @@
 public class Problem10 {
+    public static void main(String[] args) {
+        final int primes = 50;
+        final int lines = 10;
+        int count = 0;
+        int num = 2;
+        System.out.println("The first 50 prime numbers are ");
+        while (count < primes) {
+            boolean isPrime = true;
+            for (int divisor = 2; divisor <= num / 2; divisor++) {
+                if (num % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                count++;
+                if (count % lines == 0) {
+                    System.out.println(num);
+                } else
+                    System.out.print(num + " ");
+            }
+            num++;
+        }
+    }
 }
