10402373654b5f8ad9af0649735ab1555364662b
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-22 23:33:55 +0600

    Main problem solved, made mistake before dx !=dy

--- project-01/src/Problem01.java
+++ project-01/src/Problem01.java
@@ -7,8 +7,9 @@ public class Problem01 extends PApplet {
     }
 
     public void setup() {
-        background(0, 0,0);
+        background(0, 0, 0);
     }
+
     int moveX = (int) Math.floor(Math.random() * 9) + 7;
     int moveY = (int) Math.floor(Math.random() * 9) + 7;
     int dx = 0;
@@ -16,35 +17,23 @@ public class Problem01 extends PApplet {
 
     public void draw() {
 
+
         noStroke();
         rect(0, 0, width, height);
         background(0, 0, 0);
-        fill(0, 0 ,255);
-        circle(width/4f +dx, height/4f+dy,50);
-        if (dx >= width / 4f || dx <= -width / 4f){
-            moveX = -moveX;
+        fill(0, 0, 255);
+        circle(width / 4f + dx, height / 4f + dy, 50);
+        if (dx >= width / 4 - 25 || dx <= -width / 4 + 25) {
+            moveX *= -1;
         }
-        if (dx >= width / 4f || dx <= -width / 4f){
-            moveY= -moveY;
+        if (dy >= height / 4 - 25 || dy <= -height / 4 + 25) {
+            moveY *= -1;
         }
-            dx+=moveX;
-            dy+=moveY;
+        dx += moveX;
+        dy += moveY;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
+    }
 
     public static void main(String[] args) {
         PApplet.main("Problem01");
