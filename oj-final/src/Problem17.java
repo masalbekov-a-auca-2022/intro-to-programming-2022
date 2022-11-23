2038dde9830cd10f73b91cc6d0724494b729dc42
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-23 13:27:17 +0600

    OJ FINAL 17

---
+++ oj-final/src/Problem17.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int answer = 1;
+        int b = 2;
+        if (a == 0 || a ==1) {
+            System.out.println(0);
+        } else {
+            while (a > b) {
+                b *= 2;
+                answer++;
+            }
+            System.out.println(answer);
+        }
+    }
+}
