7ca33663eab978e7046ed79ccf555580aff7405b
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-28 14:28:26 +0600

    Project 04 started

---
+++ project-01/src/Problem04.java
@@ -0,0 +1,42 @@
+import processing.core.*;
+
+public class Problem04 extends PApplet {
+    static final int ROWS = 20;
+    static final int COLS = 20;
+    int LINE_COLOR = 0xFF0000FF;
+    float LINE_WEIGHT = 3.0f;
+    float CELL_GAP = 0.8f;
+
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+        background(0, 0, 0);
+        stroke(LINE_COLOR);
+        strokeWeight(LINE_WEIGHT);
+        noFill();
+        float CELL_SIZE = (Math.min(width / COLS, height / ROWS)) * (1.0f - CELL_GAP);
+        float cx = (width - ROWS * CELL_SIZE) * 0.5f;
+        float cy = (height - ROWS * CELL_SIZE) * 0.5f;
+
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                float x = cx + j * CELL_SIZE;
+                float y = cy + j * CELL_SIZE;
+                rect(x, y, CELL_SIZE, CELL_SIZE);
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Problem01");
+    }
+
+}
