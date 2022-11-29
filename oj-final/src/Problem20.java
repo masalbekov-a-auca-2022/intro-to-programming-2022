a65cc0e43c6844604c93bc0666316c198a428455
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 22:43:27 +0600

    OJ 20 ACCEPTED

--- oj-final/src/Problem20.java
+++ oj-final/src/Problem20.java
@@ -3,6 +3,27 @@ import java.util.Scanner;
 public class Problem20 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for (int i =0; i < 6;)
+        int i = 0;
+        int w =0;
+        int l=0;
+        while (i <6){
+            String a = scanner.next();
+            if (a.equals("W")){
+                w+=1;
+            } else {
+                l +=1;
+            }
+            i++;
+        }
+        if (w > 4){
+            System.out.println("1");
+        } else if (w >2){
+            System.out.println("2");
+
+        } else if (w > 0){
+            System.out.println("3");
+        } else {
+            System.out.println("-1");
+        }
     }
 }
