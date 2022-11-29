c8fc515edb51101975d8ef7b40d12c36252bcbb5
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 23:34:01 +0600

    project-01 Problem03 FINISHED

--- project-01/src/Problem03.java
+++ project-01/src/Problem03.java
@@ -1,8 +1,8 @@
 import processing.core.*;
 
 public class Problem03 extends PApplet {
-    float x = 0;
-    float y = 0;
+    float dx = 0;
+    float dy = 0;
 
     public void settings() {
         fullScreen();
@@ -14,6 +14,11 @@ public class Problem03 extends PApplet {
     public void draw() {
         background(0, 0, 0);
         noStroke();
+
+
+
+
+
         if (mouseX > width / 4f - 100 && mouseX < width / 4f + 100 &&
                 mouseY > height * 2 / 6f - 100 && mouseY < height * 2 / 6f + 100) {
             stroke(255, 254, 79);
@@ -26,11 +31,49 @@ public class Problem03 extends PApplet {
             noStroke();
         }
         fill(255, 0, 0);
-        circle(width / 4f + x, height * 2 / 6f + y, 200);
-        }
+        circle(width / 4f + dx, height * 2 / 6f + dy, 200);
 
-        public static void main (String[]args){
-            PApplet.main("Problem03");
+
+
+
+
+        if (mouseX > 620 && mouseX < 895 &&
+                mouseY > 220 && mouseY < 380) {
+            stroke(255, 254, 79);
+            strokeWeight(6);
+            fill(255, 255, 255);
+            textSize(48);
+            textAlign(CENTER);
+            text("Rectangle", width / 2f, height * 3 / 4f);
+        } else {
+            noStroke();
         }
+        fill(18, 76, 79);
+        rect(620, 220, 300, 160);
+
+
+
+
+
+        if (mouseX > width * 3 / 4f - 100 && mouseX < width * 3 / 4f + 100 &&
+                mouseY > height * 2 / 6f - 100 && mouseY < height * 2 / 6f + 100) {
+            stroke(255, 254, 79);
+            strokeWeight(6);
+            fill(255, 255, 255);
+            textSize(48);
+            textAlign(CENTER);
+            text("Ring", width / 2f, height * 3 / 4f);
+        } else {
+            noStroke();
+        }
+        fill(0, 0, 255);
+        circle(width * 3 / 4f + dx, height * 2 / 6f + dy, 200);
+        fill(0, 0, 0);
+        circle(width * 3 / 4f + dx, height * 2 / 6f + dy, 140);
 
     }
+
+    public static void main(String[] args) {
+        PApplet.main("Problem03");}
+    }
+
