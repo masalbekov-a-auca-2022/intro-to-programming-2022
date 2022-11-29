5a9c7b3c2f718495a3fcb142fd72e4eba13b1337
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 22:41:15 +0600

    OJ 21 ACCEPTED

---
+++ oj-final/src/Problem21.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;public class Problem21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int p = scanner.nextInt();
+        int n = scanner.nextInt();
+        int r = scanner.nextInt();
+        int sum = n;
+        int i = 0;
+        while (sum <= p ){
+            sum+= n*r ;
+            n*=r;
+            i++;
+        }
+        System.out.println(i);
+    }
+}
+
+
