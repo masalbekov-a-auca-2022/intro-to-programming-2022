0401bf95d9dce552c4b0e1369038e947bfd227a5
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 21:56:29 +0600

    OJ 19 ACCEPTED

---
+++ oj-final/src/Problem19.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Problem19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int m = scanner.nextInt();
+        int i =0;
+        while (i <= 101) {
+            i++;
+            if ((i * x  % m == 1)) {
+                System.out.println(i);
+                break;
+            }
+            if (i == 101) {
+                System.out.println("No such integer exists.");
+            }
+        }
+    }
+}
+
