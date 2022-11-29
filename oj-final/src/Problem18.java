2ae3fa84fabd7624739e6a989ceb57cc180c0ba1
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 22:08:39 +0600

    OJ 18 ACCEPTED

---
+++ oj-final/src/Problem18.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int a = 0;
+        boolean isTrue = true;
+
+        while (isTrue) {
+            if (Math.pow(a, 2) > num) {
+                a--;
+                isTrue = false;
+            } else {
+                a++;
+            }
+        }
+        System.out.println("The largest square has side length " + a + ".");
+    }
+}
