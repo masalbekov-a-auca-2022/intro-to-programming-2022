01146b54a234d9750bf559bf779ac94550b48fc9
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 23:23:09 +0600

    Okay circle finished

--- project-01/src/Problem03.java
+++ project-01/src/Problem03.java
@@ -16,6 +16,17 @@ public class Problem03 extends PApplet {
         noStroke();
         if (mouseX > width / 4f - 100 && mouseX < width / 4f + 100 &&
                 mouseY > height * 2 / 6f - 100 && mouseY < height * 2 / 6f + 100) {
+            stroke(255, 254, 79);
+            strokeWeight(6);
+            fill(255, 255, 255);
+            textSize(48);
+            textAlign(CENTER);
+            text("Circle", width / 2f, height * 3 / 4f);
+        } else {
+            noStroke();
+        }
+        fill(255, 0, 0);
+        circle(width / 4f + x, height * 2 / 6f + y, 200);
         }
 
         public static void main (String[]args){
@@ -23,4 +34,3 @@ public class Problem03 extends PApplet {
         }
 
     }
-}
