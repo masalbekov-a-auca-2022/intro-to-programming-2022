b03076a9e1092de8548539101f878aebacae5242
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-11-29 19:24:10 +0600

    Project 04 keyPressed

--- project-01/src/Problem04.java
+++ project-01/src/Problem04.java
@@ -5,18 +5,24 @@ public class Problem04 extends PApplet {
     static final int COLS = 20;
     int LINE_COLOR = 0xFF0000FF;
     float LINE_WEIGHT = 3.0f;
-    float CELL_GAP = 0.8f;
-
+    float CELL_GAP = 0.04f;
+    float dx = 0;
+    float dy =0;
 
     public void settings() {
         fullScreen();
     }
+    public void keyPressed(){
+
+    }
+
 
     public void setup() {
+
     }
 
     public void draw() {
-        background(0, 0, 0);
+        background(0,0,0);
         stroke(LINE_COLOR);
         strokeWeight(LINE_WEIGHT);
         noFill();
@@ -28,7 +34,7 @@ public class Problem04 extends PApplet {
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
                 float x = cx + j * CELL_SIZE;
-                float y = cy + j * CELL_SIZE;
+                float y = cy + i * CELL_SIZE;
                 rect(x, y, CELL_SIZE, CELL_SIZE);
 
             }
@@ -36,7 +42,7 @@ public class Problem04 extends PApplet {
     }
 
     public static void main(String[] args) {
-        PApplet.main("Problem01");
+        PApplet.main("Problem04");
     }
 
 }
