3b78763c20acd7f1937b46c37e44d1e533ecacf3
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-05 21:39:37 +0600

    Problem06 VENUS COMPLETED, MOON STARTED

--- project-01/src/Problem06.java
+++ project-01/src/Problem06.java
@@ -3,10 +3,17 @@ import processing.core.*;
 public class Problem06 extends PApplet {
     static final float SUN_D = 250.0f;
 
-    static final float EARTH_D = SUN_D * 0.2f;
-    static final float EARTH_ORBIT = 200.0f;
+    static final float EARTH_D = SUN_D * 0.3f;
+    static final float EARTH_ORBIT = 350.0f;
     static final float EARTH_ORBIT_SPEED = 0.02f;
     float earthOrbitAngle = 0.0f;
+
+    static final float VENUS_D = SUN_D * 0.25f;
+    static final float VENUS_ORBIT = 200.0f;
+    static final float VENUS_ORBIT_SPEED = 0.04f;
+    float venusOrbitAngle = 0.0f;
+
+
 
     public void celestialBody(float x, float y, float diam, int rColor, int gColor, int bColor, int alpha) {
         noStroke();
@@ -39,6 +46,10 @@ public class Problem06 extends PApplet {
         celestialBody(earthX, earthY, EARTH_D, 28, 170, 214, 255);
         earthOrbitAngle += EARTH_ORBIT_SPEED;
         //Venus
+        float venusX = sunX + cos(venusOrbitAngle) * VENUS_ORBIT;
+        float venusY = sunY + sin(venusOrbitAngle) * VENUS_ORBIT;
+        celestialBody(venusX, venusY, VENUS_D, 255, 102, 0, 255);
+        venusOrbitAngle += VENUS_ORBIT_SPEED;
 
         //TODO
 
