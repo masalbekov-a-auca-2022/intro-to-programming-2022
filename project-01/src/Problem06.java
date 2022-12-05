c2518665a04337ae545bbded052b7fade4677872
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-05 21:32:39 +0600

    Problem06 COLORS IN RGB, Started Venus

--- project-01/src/Problem06.java
+++ project-01/src/Problem06.java
@@ -2,17 +2,15 @@ import processing.core.*;
 
 public class Problem06 extends PApplet {
     static final float SUN_D = 250.0f;
-    static final int SUN_COLOR = 0xFFFFAA00;
 
     static final float EARTH_D = SUN_D * 0.2f;
-    static final int EARTH_COLOR = 0xFF007BFF;
     static final float EARTH_ORBIT = 200.0f;
     static final float EARTH_ORBIT_SPEED = 0.02f;
     float earthOrbitAngle = 0.0f;
 
-    public void celestialBody(float x, float y, float diam, int color) {
+    public void celestialBody(float x, float y, float diam, int rColor, int gColor, int bColor, int alpha) {
         noStroke();
-        fill(color);
+        fill(rColor, gColor, bColor, alpha);
         circle(x, y, diam);
 
 
@@ -30,16 +28,17 @@ public class Problem06 extends PApplet {
         background(0, 0, 0);
 
 
-        // Sun
+        //Sun
         float sunX = width * 0.5f;
         float sunY = height * 0.5f;
-        celestialBody(sunX, sunY, SUN_D, SUN_COLOR);
+        celestialBody(sunX, sunY, SUN_D, 255, 255, 0, 255 );
 
         //Earth
         float earthX = sunX + cos(earthOrbitAngle) * EARTH_ORBIT;
         float earthY = sunY + sin(earthOrbitAngle) * EARTH_ORBIT;
-        celestialBody(earthX, earthY, EARTH_D, EARTH_COLOR);
+        celestialBody(earthX, earthY, EARTH_D, 28, 170, 214, 255);
         earthOrbitAngle += EARTH_ORBIT_SPEED;
+        //Venus
 
         //TODO
 
