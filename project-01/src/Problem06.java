bf967321f36ce604650ae8df14fbba44a05cf4f5
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-05 21:11:08 +0600

    Problem06 EARTH AND SUN

--- project-01/src/Problem06.java
+++ project-01/src/Problem06.java
@@ -6,6 +6,18 @@ public class Problem06 extends PApplet {
 
     static final float EARTH_D = SUN_D * 0.2f;
     static final int EARTH_COLOR = 0xFF007BFF;
+    static final float EARTH_ORBIT = 200.0f;
+    static final float EARTH_ORBIT_SPEED = 0.02f;
+    float earthOrbitAngle = 0.0f;
+
+    public void celestialBody(float x, float y, float diam, int color){
+        noStroke();
+        fill(color);
+        circle(x, y, diam);
+
+
+    }
+
 
     public void settings() {
         fullScreen();
@@ -16,6 +28,24 @@ public class Problem06 extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
+
+
+        // Sun
+        float sunX = width*0.5f;
+        float sunY = height*0.5f;
+        celestialBody(sunX, sunY, SUN_D, SUN_COLOR);
+
+        //Earth
+
+        float earthX = width*0.5f;
+        float earthY =height*0.5f;
+        celestialBody(earthX, earthY, EARTH_D, EARTH_COLOR);
+        earthOrbitAngle +=EARTH_ORBIT_SPEED;
+
+        //TODO
+
+
+
     }
 
     public static void main(String[] args) {
