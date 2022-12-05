cc764ecb3103e0f4f430d5a0df02afa3d295eea0
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-05 14:28:08 +0600

    OJ FINAL 27 ACCEPTED

---
+++ oj-final/src/Problem27.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem27 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int counter = 0;
+        for (int i = 1; i <= a - 3; i++) {
+            for (int j = i + 1; j <= a - 2; j++) {
+                for (int k = j + 1; k <= a - 1; k++) {
+                    counter ++;
+                }
+            }
+        }
+        System.out.println(counter);
+    }
+}
+
