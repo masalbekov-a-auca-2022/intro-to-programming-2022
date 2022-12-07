1b60cb3c12c0990ef976310e26352f5df42525ae
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-07 13:14:49 +0600

    Problem01

--- lab-06/src/Problem01.java
+++ lab-06/src/Problem01.java
@@ -3,12 +3,29 @@ import java.util.Scanner;
 public class Problem01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Your coordinate: ");
         int a = scanner.nextInt();
+        System.out.println("Coordinate of 1st point: ");
         int b = scanner.nextInt();
+        System.out.println("Coordinate of 2nd point: ");
         int c = scanner.nextInt();
-        int d1 = (a - b);
-        int d2 = a - c;
+        int d1 = abs(b - a);
+        int d2 = abs(c - a);
+
+        if (d1 < d2) {
+            System.out.println("1st is closer. distance is " + d1);
+        } else if (d2 < d1) {
+            System.out.println("2nd is closer. Distance is " + d2);
+        } else {
+            System.out.println("distance is the same: " + d1);
+        }
 
     }
-}
 
+    static int abs(int n) {
+        if (n < 0) {
+            n = -n;
+        }
+        return n;
+    }
+}
