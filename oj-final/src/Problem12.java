9d41db1ddf9c334e29dd7fea8128e6a4be2f08d2
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-12 13:33:02 +0600

    OJ FINAL 12 ACCEPTED

--- oj-final/src/Problem12.java
+++ oj-final/src/Problem12.java
@@ -4,12 +4,12 @@ public class Problem12 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int max_bid =0;
+        int max_bid = 0;
         String winner = "";
-        for (int i =0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             String name = scanner.next();
             int bid = scanner.nextInt();
-            if (bid > max_bid){
+            if (bid > max_bid) {
                 max_bid = bid;
                 winner = name;
             }
---
+++ oj-final/src/Problem13.java
@@ -0,0 +1,5 @@
+public class Problem13 {
+    public static void main(String[] args) {
+
+    }
+}
