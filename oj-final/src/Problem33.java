0bd458c989626cac01bb26c806ee3174c17182c5
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-16 19:02:15 +0600

    OJ FINAL 33 ACCEPTED

--- oj-final/src/Problem33.java
+++ oj-final/src/Problem33.java
@@ -4,11 +4,11 @@ public class Problem33 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        for (int i =0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int y = scanner.nextInt();
             int m = scanner.nextInt();
             int d = scanner.nextInt();
-            if (y % 100 != 0 && y % 4 == 0 || y % 400 == 0){
+            if (y % 100 != 0 && y % 4 == 0 || y % 400 == 0) {
 
             }
         }
@@ -16,9 +16,6 @@ public class Problem33 {
     //public static int getNumberOfDays(int m, int y, int d) {
 
 
-
-
-
-    }
+}
 
 //}
