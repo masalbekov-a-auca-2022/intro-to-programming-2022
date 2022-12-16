0ba1a94633c3903778e5a81cf80ba3901d4337e4
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-16 15:15:27 +0600

    OJ FINAL 34 ACCEPTED

---
+++ oj-final/src/Problem34.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem34 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i =0; i < n; i++){
+            int a = scanner.nextInt();
+            System.out.println(isPrime(a) ? 1 : 0);
+        }
+
+    }
+    public static boolean isPrime(int a){
+        for (int j = 2; j < a; j++){
+            if (a % j ==0) return false;
+        }
+        return true;
+    }
+}
