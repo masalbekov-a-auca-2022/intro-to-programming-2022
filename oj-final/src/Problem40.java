e965cdfaad5ed924796b6e4d753d55570ebb0230
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-16 19:01:45 +0600

    OJ FINAL 38 ACCEPTED

---
+++ oj-final/src/Problem40.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Problem40 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+    }
+}
