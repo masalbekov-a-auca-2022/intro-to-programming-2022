2556eb09816a582169206ea3d5b0218b48cfd97a
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-20 20:01:17 +0600

    Problem05

--- lab-06/src/Problem04.java
+++ lab-06/src/Problem04.java
@@ -1,3 +1,29 @@
-public class Problem04 {
+import java.util.Scanner;
 
+public class Problem04 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("A = ");
+        int num1 = scanner.nextInt();
+        System.out.print("B = ");
+        int num2 = scanner.nextInt();
+        if (gcd(num1, num2) > 0) {
+            System.out.println("GCD(" + num1 + ", " + num2 + ") = " + gcd(num1, num2));
+        } else {
+            System.out.println("GCD(" + num1 + ", " + num2 + ") is not defined.");
+        }
+    }
+
+    static int gcd(int num1, int num2) {
+        int gcd = 0;
+        for (int i = 1; i < Math.max(num1, num2); ++i) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                if (gcd < i) {
+                    gcd = i;
+                }
+            }
+        }
+        return gcd;
+    }
 }
--- lab-06/src/Problem05.java
+++ lab-06/src/Problem05.java
@@ -1,20 +1,28 @@
-import processing.core.*;
+import java.util.Scanner;
 
-public class Problem05 extends PApplet {
-
-    public void settings() {
-        fullScreen();
-    }
-
-    public void setup() {
-    }
-
-    public void draw() {
-        background(0, 0, 0);
-    }
+public class Problem05 {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        PApplet.main("Problem05");
+        System.out.print("A = ");
+        int num1 = scanner.nextInt();
+        System.out.print("B = ");
+        int num2 = scanner.nextInt();
+        if (gcd(num1, num2) > 0) {
+            System.out.println("GCD(" + num1 + ", " + num2 + ") = " + gcd(num1, num2));
+        } else {
+            System.out.println("GCD(" + num1 + ", " + num2 + ") is not defined.");
+        }
     }
 
+    static int gcd(int num1, int num2) {
+        int gcd = 0;
+        while (num2 > 0) {
+            int rem = num1 % num2;
+            num1 = num2;
+            num2 = rem;
+        }
+        gcd = num1;
+        return gcd;
+    }
 }
