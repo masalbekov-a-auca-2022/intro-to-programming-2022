40f6bc62f8482d98f14793c78c5779075bda91e1
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-20 19:19:57 +0600

    OJ FINAL 40 ACCEPTED

--- oj-final/src/Problem40.java
+++ oj-final/src/Problem40.java
@@ -3,7 +3,24 @@ import java.util.Scanner;
 public class Problem40 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
 
+        int adj = scanner.nextInt();
+        int noun = scanner.nextInt();
+
+        String[] adjective = new String[adj];
+        String[] nouns = new String[noun];
+
+        for (int i = 0; i < adj; i++) {
+            adjective[i] = scanner.next();
+        }
+        for (int i = 0; i < noun; i++) {
+            nouns[i] = scanner.next();
+        }
+
+        for (int i = 0; i < adj; i++) {
+            for (int j = 0; j < noun; j++) {
+                System.out.println(adjective[i] + " as " + nouns[j]);
+            }
+        }
     }
 }
