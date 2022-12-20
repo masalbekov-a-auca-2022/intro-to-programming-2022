ac5342ac0bf36eded14ce139432c612643b07071
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-20 13:48:23 +0600

    Problem08

---
+++ lab-06/src/Problem08.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+    }
+}
