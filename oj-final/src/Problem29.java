f9e92e16cfb294fce0f7fab0287664ed5677bcb6
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-20 19:13:30 +0600

    Problem29

--- oj-final/src/Problem29.java
+++ oj-final/src/Problem29.java
@@ -2,7 +2,25 @@ import java.util.Scanner;
 
 public class Problem29 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        while(n != 0) {
+            int minP = 65000;
+            int minX = 0;
+            int minY = 0;
+            for (int i = 1; i <= Math.sqrt(n); i++) {
+                for (int j = i; j <= n; j++) {
+                    if(i * j == n) {
+                        if(2 * (i + j) < minP) {
+                            minP = 2 * (i + j);
+                            minX = i;
+                            minY = j;
+                        }
+                    }
+                }
+            }
+            System.out.printf("Minimum perimeter is %d with dimensions %d x %d%n", minP, minX, minY);
+            n = input.nextInt();
+        }
     }
 }
