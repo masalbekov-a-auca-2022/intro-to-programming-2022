c8a3ef5cb52e7ba85701f1acd83682fe8ef32a8f
Aziret Masalbekov <masalbekov_a@auca.kg>
2022-12-20 21:20:44 +0600

    Problem03

--- lab-07/src/Problem03.java
+++ lab-07/src/Problem03.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class Problem03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("N: ");
+        System.out.print("N:");
         int a = scanner.nextInt();
         int[] c = new int[a];
         for (int i = 0; i < a; i++) {
+            System.out.printf("Enter %d element: ",i );
             int b = scanner.nextInt();
             c[i] = b;
         }
-        System.out.printf(reverseArray(c));
+        System.out.println("Before reversing:");
+        for (int j =0; j < c.length; j++){
+            System.out.print(c[j] + " ");
+        }
+        System.out.println();
+        reverseArray(c);
+        System.out.println("After reversing:");
+        for (int k = 0; k < c.length; k++) {
+            System.out.print(c[k] + " ");
+        }
 
     }
 
