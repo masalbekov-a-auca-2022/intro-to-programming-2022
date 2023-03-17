9ae16fb46dba0d9f4e7dfaf84842e17aab253fb5
Aziret Masalbekov <masalbekov_a@auca.kg>
2023-03-17 20:02:03 +0600

    URI1273 PRESENTATION ERROR

--- uri-midterm/src/TEST.java
+++ uri-midterm/src/TEST.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
 
-public class URI1273 {
+public class TEST {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int q = 0;
         while (true) {
             int n = scanner.nextInt();
-            q++;
             if (n == 0) {
                 break;
-            } else if (q != 1) {
+            }
+            q++;
+            if (q != 1) {
                 System.out.println();
             }
             scanner.nextLine();
-            int a = 0;
-            String[] c = new String[n];
+            String[] words = new String[n];
+            int longestWordLength = 0;
             for (int i = 0; i < n; i++) {
-                String h = scanner.nextLine();
-                c[i] = scanner.nextLine().trim();
-                a = Math.max(a, c[i].length());
+                words[i] = scanner.nextLine().trim();
+                longestWordLength = Math.max(longestWordLength, words[i].length());
             }
-            for (String h : c) {
-                int numSpacesToAdd = a - h.length();
+            for (String word : words) {
+                int numSpacesToAdd = longestWordLength - word.length();
                 if (numSpacesToAdd > 0) {
                     for (int i = 0; i < numSpacesToAdd; i++) {
                         System.out.print(" ");
                     }
                 }
-                System.out.println(h);
+                System.out.println(word);
             }
-
         }
     }
 }
